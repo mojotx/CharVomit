33a4839711968ba6683af5f45bf96d0735dffe01
Michael Jarvis <michael@jarvis.com>
2021-06-02 17:21:54 -0500

    Tweak some things

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/mojotx/CharVomit/pkg/CharVomit"
+	"os"
+	"strconv"
 )
 
+// TO-DO:
+// - Add support for duplicate character checking
+// - Add support for specifying valid characters, e.g., all upper-case, etc.
 func main() {
 	cv := CharVomit.NewCharVomit(CharVomit.DefaultChars)
 
-	pw, err := cv.Puke(32)
+	pwLen := 32
+
+	if len(os.Args) == 2 {
+
+		var err error
+		pwLen, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if pwLen < 0 {
+			pwLen = pwLen * -1
+		}
+	}
+	pw, err := cv.Puke(pwLen)
 	if err != nil {
 		panic(err.Error())
 	}
