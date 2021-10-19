6e19f306ea4bde5949d808c282883a9a6327c54b
Michael Jarvis <michael@jarvis.com>
2021-10-19 11:05:07 -0500

    Move password length parsing here

--- pkg/arg/parse.go
+++ pkg/arg/parse.go
@@ -4,28 +4,33 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type ConfigType struct {
-	Args      int
-	Digits    bool
-	ShowHelp  bool
-	LowerCase bool
-	Symbols   bool
-	UpperCase bool
-	WeakChars bool
+	PasswordLen int
+	Digits      bool
+	ShowHelp    bool
+	LowerCase   bool
+	Symbols     bool
+	UpperCase   bool
+	WeakChars   bool
 }
 
 var Config ConfigType
 
 func Usage() {
 	out := flag.CommandLine.Output()
-	_, _ = fmt.Fprintf(out, "Usage: %s\n\n", os.Args[0])
-	_, _ = fmt.Fprintf(out, "Options:\n")
+	_, _ = fmt.Fprintf(out, "Usage: %s [ length ]\n\n", os.Args[0])
+	_, _ = fmt.Fprintf(out, "If a password length is not specified, 32 is used.\n\n")
+	_, _ = fmt.Fprintf(out, "Other optional flags are:\n")
 
 	flag.PrintDefaults()
 
 	_, _ = fmt.Fprintln(out)
+	_, _ = fmt.Fprintf(out, "Note that optional flags must precede the password length.\n\n")
+	_, _ = fmt.Fprintf(out, "For example, a 8-character password of all capital letters:\n")
+	_, _ = fmt.Fprintf(out, "%s -u 8\n\n", os.Args[0])
 
 }
 
@@ -40,6 +45,22 @@ func Parse() {
 
 	flag.Parse()
 
-	Config.Args = flag.NArg()
+	if flag.NArg() == 1 {
+
+		var err error
+		Config.PasswordLen, err = strconv.Atoi(flag.Arg(0))
+		if err != nil {
+			_, _ = fmt.Fprintf(flag.CommandLine.Output(), "cannot parse argument '%+v': %s", flag.Arg(0), err.Error())
+			os.Exit(1)
+		}
+
+		// Get absolute value
+		if Config.PasswordLen < 0 {
+			Config.PasswordLen = Config.PasswordLen * -1
+		}
+	} else {
+		// default to 32 characters
+		Config.PasswordLen = 32
+	}
 
 }
