329607adffaf73cfb160a68898f8de2e41d53da1
Michael Jarvis <michael@jarvis.com>
2022-03-09 14:56:37 -0600

    Add some package documentation.

---
+++ pkg/CharVomit/doc.go
@@ -0,0 +1,6 @@
+// Package CharVomit provides functionality to generate large
+// pseudo-random passwords, that look as if a cat puked up random
+// characters. The code uses crypto/rand for random number generation,
+// but does allow you to eliminate characters that are confusing, such
+// as the digit '0' and the capital letter 'O'.
+package CharVomit
---
+++ pkg/arg/doc.go
@@ -0,0 +1,2 @@
+// Package arg is a helper package for parsing command-line arguments.
+package arg
