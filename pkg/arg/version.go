0af507818e0062245f14ebc2834944a0df1f1951
Michael Jarvis <michael@jarvis.com>
2022-03-15 10:44:28 -0500

    Releane v1.3.2

--- pkg/arg/version.go
+++ pkg/arg/version.go
@@ -2,7 +2,7 @@ package arg
 
 const (
 	// CharVomitVersion is the current version of the application
-	CharVomitVersion = "v1.3.1"
+	CharVomitVersion = "v1.3.2"
 )
 
 // Version returns the current version of the application
