e17e9090d5cfd429e598bb22fb5e1fbb9d300e2d
Michael Jarvis <michael@jarvis.com>
2022-03-15 11:20:03 -0500

    Fix EXTRA_FILES, CMD_PATH

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 export GOPATH=$HOME/go
+
+if [[ -z "${CMD_PATH}" ]]; then
+  CMD_PATH="./..."
+  fi
+
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
-    go test ./... 1> debug.out
+    go test "${CMD_PATH}" 1> debug.out
 else
-    go test -v ./...
+    go test -v "${CMD_PATH}"
 fi
 
 
 BINARY="CharVomit"
 
-if go get ./... 1>> debug.out; then
+if go get "${CMD_PATH}" 1>> debug.out; then
     if [ "${GOOS}" == "windows" ]; then
         if [ "${GITHUB_ACTIONS}" == "true" ]; then
             go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o ${BINARY}.exe ./cmd/CharVomit 1>> debug.out
