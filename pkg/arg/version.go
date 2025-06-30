de5c5188d659cf05da8080e3423f53f3df5d5837
Michael Jarvis <michael@jarvis.com>
2025-06-30 12:23:23 -0500

    bump version

--- pkg/arg/version.go
+++ pkg/arg/version.go
@@ -2,7 +2,7 @@ package arg
 
 const (
 	// CharVomitVersion is the current version of the application
-	CharVomitVersion = "v1.5.3"
+	CharVomitVersion = "v1.5.5"
 )
 
 // Version returns the current version of the application
